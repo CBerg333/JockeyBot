3c2e8130662275b24da77fa7785dde4611497451
Christopher McGarrity <come24will@gmail.com>
2024-10-07 17:18:29 -0400

    Added audio file for testing, initialized pyaudio

--- jockeymain.py
+++ jockeymain.py
@@ -1,11 +1,14 @@
 import discord
 import ffmpeg
 from discord.ext import commands
+import pyaudio
 
-
-intents = discord.intents.default()
-intents.message.content = True
+intents = discord.Intents.default()
 bot = commands.Bot(command_prefix=",", intents=intents)
+input_stream = ffmpeg.input('congo cut cut.mp3')
+
+play = pyaudio.PyAudio()
+
 
 @bot.event
 async def on_ready():
